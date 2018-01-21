d71e0ce1813c658d8ba2108cd3677350da9d538d
Abhishanth Padarthy <abhipadarthy@gmail.com>
2018-01-21 14:09:12 +0530

    Changed StopWatch code.
    Signed-off-by: Abhishanth Padarthy <abhipadarthy@gmail.com>

--- app/src/main/java/me/iologic/apps/dtn/StopWatch.java
+++ app/src/main/java/me/iologic/apps/dtn/StopWatch.java
@@ -1,59 +1,105 @@
 package me.iologic.apps.dtn;
 
-import android.os.SystemClock;
-
-/**
- * Created by vinee on 20-01-2018.
- */
-
-import android.view.View;
-import android.widget.ArrayAdapter;
+import android.os.Bundle;
 import android.widget.TextView;
 import android.os.Handler;
+import android.view.View;
 
-public class StopWatch extends Thread {
-    long MillisecondTime, startTime, timeBuf, updateTime=0L;
+class StopWatch extends Thread {
+    private TextView tempTextView; //Temporary TextView
+//private Button tempBtn; //Temporary Button
+    private Handler mHandler = new Handler();
+    private long startTime;
+    private long elapsedTime;
+    private final int REFRESH_RATE = 100;
+    private String hours, minutes, seconds, milliseconds;
+    private long secs, mins, hrs, msecs;
+    private boolean stopped = false;
 
-    private java.util.logging.Handler sHandler = new Handler();
+    private void updateTimer(float time) {
+        secs = (long) (time / 1000);
+        mins = (long) ((time / 1000) / 60);
+        hrs = (long) (((time / 1000) / 60) / 60);
 
-    int seconds, minutes, milliseconds;
+		/* Convert the seconds to String
+		 * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs = secs % 60;
+        seconds = String.valueOf(secs);
+        if (secs == 0) {
+            seconds = "00";
+        }
+        if (secs < 10 && secs > 0) {
+            seconds = "0" + seconds;
+        }
 
-    public StopWatch(Handler handler){
+		/* Convert the minutes to String and format the String */
 
+        mins = mins % 60;
+        minutes = String.valueOf(mins);
+        if (mins == 0) {
+            minutes = "00";
+        }
+        if (mins < 10 && mins > 0) {
+            minutes = "0" + minutes;
+        }
+
+    	/* Convert the hours to String and format the String */
+
+        hours = String.valueOf(hrs);
+        if (hrs == 0) {
+            hours = "00";
+        }
+        if (hrs < 10 && hrs > 0) {
+            hours = "0" + hours;
+        }
+
+    	/* Although we are not using milliseconds on the timer in this example
+    	 * I included the code in the event that you wanted to include it on your own
+    	 */
+        milliseconds = String.valueOf((long) time);
+        if (milliseconds.length() == 2) {
+            milliseconds = "0" + milliseconds;
+        }
+        if (milliseconds.length() <= 1) {
+            milliseconds = "00";
+        }
+        milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
+
+		/* Setting the timer text to the elapsed time */
+        ((TextView) findViewById(R.id.timer)).setText(hours + ":" + minutes + ":" + seconds);
+        ((TextView) findViewById(R.id.timerMs)).setText("." + milliseconds);
     }
 
-    public void start(){
-        startTime = SystemClock.uptimeMillis();
-        handler
-    }
-
-    public void pause(){
-        timeBuf += MillisecondTime;
-        handler
-    }
-
-    public void reset(){
-        MillisecondTime = 0L ;
-        startTime = 0L ;
-        timeBuf = 0L ;
-        updateTime = 0L ;
-        seconds = 0 ;
-        minutes = 0 ;
-        milliseconds = 0 ;
-
-        textView.setText("00:00:00");
-    }
-
-    public Runnable runnable = new Runnable() {
+    private Runnable startTimer = new Runnable() {
         public void run() {
-            MillisecondTime = SystemClock.uptimeMillis() - startTime;
-            updateTime = timeBuf + MillisecondTime;
-            seconds = (int) (updateTime / 1000);
-            minutes = seconds / 60;
-            seconds = seconds % 60;
-            milliseconds = (int) (updateTime % 1000);
-            textView.setText("" + minutes + ":" + String.format("%02d", seconds) + ":" + String.format("%03d", milliseconds));
-            handler.postDelayed(this, 0);
+            elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            mHandler.postDelayed(this,REFRESH_RATE);
         }
     };
+
+    public void start (View view){
+        if(stopped){
+            startTime = System.currentTimeMillis() - elapsedTime;
+        }
+        else{
+            startTime = System.currentTimeMillis();
+        }
+        mHandler.removeCallbacks(startTimer);
+        mHandler.postDelayed(startTimer, 0);
+    }
+
+    public void stop (View view){
+        mHandler.removeCallbacks(startTimer);
+        stopped = true;
+    }
+
+    public void reset (View view){
+        stopped = false;
+        ((TextView)findViewById(R.id.timer)).setText("00:00:00");
+        ((TextView)findViewById(R.id.timerMs)).setText(".0");
+    }
+
 }
