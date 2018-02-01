046d7638d53be2fe426ab470a3747a37ed3b7c6a
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-02-01 12:07:13 +0530

    Fix Error Item List
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- app/src/main/java/me/iologic/apps/dtn/MainActivity.java
+++ app/src/main/java/me/iologic/apps/dtn/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,21 +35,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(discoverableIntent);
 
         object = new OneScenario();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void openOneScenario(View view) {
