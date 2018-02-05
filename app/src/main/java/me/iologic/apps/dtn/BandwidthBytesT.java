2242b6fbc2e3625cab03a7d8a060e492d0c89629
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-02-05 22:42:10 +0530

    Checking if 32 KB BW works
    16 KB is too fast
    
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- app/src/main/java/me/iologic/apps/dtn/BandwidthBytesT.java
+++ app/src/main/java/me/iologic/apps/dtn/BandwidthBytesT.java
@@ -142,7 +142,7 @@ public class BandwidthBytesT extends Thread {
 
         byte[] sendData; // Breaking 1 MB file into 128 KB packets. So total 8 packets.
         int startPacketIndex = 0;
-        while (counter != (Constants.Packet.BW_COUNTER + 1)) { // 64 Packets
+        while (counter != (Constants.Packet.BW_COUNTER + 1)) { // 32 Packets
             sendData = Arrays.copyOfRange(getData, startPacketIndex, (startPacketIndex + Constants.Packet.BW_PACKET_SIZE) - 1);
             write(sendData);
             counter++;
