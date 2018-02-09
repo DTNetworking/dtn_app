9dcd5f0c4faa9e049b6963f6f5771d6d417ead88
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-02-09 22:58:44 +0530

    Check BW For 32 KB with LOG
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- app/src/main/java/me/iologic/apps/dtn/BandwidthBytesT.java
+++ app/src/main/java/me/iologic/apps/dtn/BandwidthBytesT.java
@@ -72,7 +72,7 @@ public class BandwidthBytesT extends Thread {
                     // Read from the InputStream.
                     numBytes = bandwidthInStream.read(bandwidthBuffer);
                     // Send the obtained bytes to the UI activity.
-                 //   Log.i(Constants.TAG, "Number Of Speed Bytes Received: " + numBytes);
+                    Log.i(Constants.TAG, "Number Of Speed Bytes Received: " + numBytes);
                     Message readMsg = bandwidthHandler.obtainMessage(
                             Constants.MessageConstants.BW_READ, numBytes, -1,
                             bandwidthBuffer);
@@ -95,7 +95,7 @@ public class BandwidthBytesT extends Thread {
             String testMessage = new String(bandwidthBuffer);
             //  Log.i(Constants.TAG, "BW Sending: " + testMessage);
 
-            if(isFirstTime) {
+            if (isFirstTime) {
                 isFirstTime = false;
                 // Share the sent message with the UI activity.
                 Message writtenBWStatus = bandwidthHandler.obtainMessage(
