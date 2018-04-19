4e28450a7e293f93c6460f2a01a8c87d1fa84776
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-04-19 18:27:49 +0530

    Added missing break; statement

--- app/src/main/java/me/iologic/apps/dtn/UUIDManager.java
+++ app/src/main/java/me/iologic/apps/dtn/UUIDManager.java
@@ -1,5 +1,7 @@
 package me.iologic.apps.dtn;
 
+import android.util.Log;
+
 import java.util.UUID;
 
 public class UUIDManager {
@@ -22,12 +24,16 @@ public class UUIDManager {
                 mmSocket_UUID = Constants.UUIDs.mmSocket_UUID;
                 ACK_UUID = Constants.UUIDs.ACK_UUID;
                 BW_UUID = Constants.UUIDs.BW_UUID;
+                break;
             case Constants.DeviceNames.destinationDevice:
                 mmSocket_UUID = Constants.UUIDs.destination_MMSocket_UUID;
                 ACK_UUID = Constants.UUIDs.destination_ACK_UUID;
                 BW_UUID = Constants.UUIDs.destination_BW_UUID;
+                break;
             default:
                 break;
         }
+
+        Log.i(Constants.TAG, "mmSocket: " + mmSocket_UUID);
     }
 }
