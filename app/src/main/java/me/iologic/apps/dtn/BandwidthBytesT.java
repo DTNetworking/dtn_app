03bcf8a4fc4c683e8e3aa803445ad75351f640d2
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-04-24 00:31:10 +0530

    Bluetooth Test to be done infinitely

--- app/src/main/java/me/iologic/apps/dtn/BandwidthBytesT.java
+++ app/src/main/java/me/iologic/apps/dtn/BandwidthBytesT.java
@@ -150,7 +150,7 @@ public class BandwidthBytesT extends Thread {
 //            counter = 1; // Reset Counter to 1
 //        }
 
-        for (int i = 0; i < 10; i++) {
+        while(true){
             write(getData);
         }
     }
