e301875a46250dc76dc8e7b752724c6f1e30f120
Abhishanth Padarthy <abhipadarthy@gmail.com>
2018-04-28 16:05:47 +0530

    Removed public static final for all variables

--- app/src/main/java/me/iologic/apps/dtn/Constants.java
+++ app/src/main/java/me/iologic/apps/dtn/Constants.java
@@ -8,40 +8,40 @@ import java.util.UUID;
 
 public class Constants {
 
-    public static final String NAME = "DTNApp";
-    public static final String TAG = "DTNLogs";
-    public static final String testFileName = "testFile";
+    String NAME = "DTNApp";
+    String TAG = "DTNLogs";
+    String testFileName = "testFile";
 
 
     public interface Earth {
-        public static final double RADIUS = 6371000;
+        double RADIUS = 6371000;
     }
 
     public interface FileNames {
-        public static final String Bandwidth = "Bandwidth";
-        public static final String Delay = "MsgTimings";
-        public static final String Pairing = "PairingTime";
-        public static final String MsgPacketLoss = "MsgPacketLoss";
-        public static final String BWPacketLoss = "BWPacketLoss";
-        public static final String Speed = "LightningMcQueen";
-        public static final String SentMessage = "SentMessage";
-        public static final String ReceivedMessage = "ReceivedMessage";
-        public static final String receivedImageFileName = "Img";
-        public static final String InterContactTime = "InterContactTime";
+        String Bandwidth = "Bandwidth";
+        String Delay = "MsgTimings";
+        String Pairing = "PairingTime";
+        String MsgPacketLoss = "MsgPacketLoss";
+        String BWPacketLoss = "BWPacketLoss";
+        String Speed = "LightningMcQueen";
+        String SentMessage = "SentMessage";
+        String ReceivedMessage = "ReceivedMessage";
+        String receivedImageFileName = "Img";
+        String InterContactTime = "InterContactTime";
     }
 
     public interface Packet {
-        public static final int PACKET_SIZE = 2; // 2 Bytes Per Packet.
-        public static final int NO_OF_PACKETS = 25;
-        public static final int MSG_PACKET_SIZE = 16; // 16 bytes. For testing packet loss in 1st Scenario.
-        public static final int BW_PACKET_SIZE = 1024 * 10; // 10 KB
-        public static final int BW_FILE_SIZE = 1024 * 10;
-        public static final int BW_COUNTER = BW_FILE_SIZE / BW_PACKET_SIZE;
+        int PACKET_SIZE = 2; // 2 Bytes Per Packet.
+        int NO_OF_PACKETS = 25;
+        int MSG_PACKET_SIZE = 16; // 16 bytes. For testing packet loss in 1st Scenario.
+        int BW_PACKET_SIZE = 1024 * 10; // 10 KB
+        int BW_FILE_SIZE = 1024 * 10;
+        int BW_COUNTER = BW_FILE_SIZE / BW_PACKET_SIZE;
     }
 
     public interface DataTypes {
-        public static final String TEXT = "Text";
-        public static final String IMAGE = "Image";
+        String TEXT = "Text";
+        String IMAGE = "Image";
         String AUDIO = "Audio";
     }
 
@@ -55,72 +55,72 @@ public class Constants {
     // Defines several constants used when transmitting messages between the
     // service and the UI.
     public interface MessageConstants {
-        public static final int MESSAGE_READ = 0;
-        public static final int MESSAGE_WRITE = 1;
-        public static final int MESSAGE_TOAST = 2;
+        int MESSAGE_READ = 0;
+        int MESSAGE_WRITE = 1;
+        int MESSAGE_TOAST = 2;
 
-        public static final int ACK_READ = 5;
-        public static final int ACK_WRITE = 6;
-        public static final int ACK_FAIL_TO_SEND = 7;
+        int ACK_READ = 5;
+        int ACK_WRITE = 6;
+        int ACK_FAIL_TO_SEND = 7;
 
-        public static final int BW_READ = 10;
-        public static final int BW_WRITE = 11;
-        public static final int BW_FAIL_TO_SEND = 12;
-        public static final int BW_START_WRITE = 14;
-        public static final int BW_PACKET_LOSS_CHECK = 15;
-        public static final String BW_PACKET_RECEIVED = "B";
+        int BW_READ = 10;
+        int BW_WRITE = 11;
+        int BW_FAIL_TO_SEND = 12;
+        int BW_START_WRITE = 14;
+        int BW_PACKET_LOSS_CHECK = 15;
+        String BW_PACKET_RECEIVED = "B";
 
-        public static final String DISCOVERY_SUCCESS_MESSAGE = "Discoverability set to ON";
-        public static final String DISCOVERY_FAIL_MESSAGE = "Discoverability failed";
+        String DISCOVERY_SUCCESS_MESSAGE = "Discoverability set to ON";
+        String DISCOVERY_FAIL_MESSAGE = "Discoverability failed";
 
-        public static final String ACK_CONNECT_SERVER_SUCCESS = "Acknowledgement connection is successful as a server";
-        public static final String ACK_CONNECT_CLIENT_SUCCESS = "Acknowledgement connection is successful as a client";
+        String ACK_CONNECT_SERVER_SUCCESS = "Acknowledgement connection is successful as a server";
+        String ACK_CONNECT_CLIENT_SUCCESS = "Acknowledgement connection is successful as a client";
 
-        public static final String BW_CONNECT_SERVER_SUCCESS = "Bandwidth connection is successful as a server";
-        public static final String BW_CONNECT_CLIENT_SUCCESS = "Bandwidth connection is successful as a client";
+        String BW_CONNECT_SERVER_SUCCESS = "Bandwidth connection is successful as a server";
+        String BW_CONNECT_CLIENT_SUCCESS = "Bandwidth connection is successful as a client";
 
-        public static final String CLIENT_CONNECTION_FAIL = "Client Connection Failed!";
+        String CLIENT_CONNECTION_FAIL = "Client Connection Failed!";
 
-        public static final String SERVER_CONNECTION_SUCCESSFUL = "Server is successfully connected!";
-        public static final String SERVER_CONNECTION_FAIL = "Server failed to connect";
+        String SERVER_CONNECTION_SUCCESSFUL = "Server is successfully connected!";
+        String SERVER_CONNECTION_FAIL = "Server failed to connect";
 
         String NOT_YET_CONNECTED = "I am not yet connected to any phone";
         String MESSAGE_IS_EMPTY = "Message is empty. Enter a message!";
     }
 
     public interface EmulationMessages {
-        public static final String CLIENTCONNECT_NOT_CONNECTED = "Socket is not open yet";
-        public static final String CLIENTCONNECT_GETTING_DISCONNECTED = "The main socket which is used to send/receive messages is being disconnected now. I will try to re-connect.";
+        String CLIENTCONNECT_NOT_CONNECTED = "Socket is not open yet";
+        String CLIENTCONNECT_GETTING_DISCONNECTED = "The main socket which is used to send/receive messages is being disconnected now. I will try to re-connect.";
     }
 
     public interface UUIDs {
-        public static final UUID mmSocket_UUID = UUID.fromString("6e7bd336-5676-407e-a41c-0691e1964345"); // UUID is uniquely generated
-        public static final UUID ACK_UUID = UUID.fromString("b03901e4-710c-4509-9718-a3d15882d050");
-        public static final UUID BW_UUID = UUID.fromString("aa401ee7-3bb2-410c-9dda-2128726513a1");
+        UUID mmSocket_UUID = UUID.fromString("6e7bd336-5676-407e-a41c-0691e1964345"); // UUID is uniquely generated
+        UUID ACK_UUID = UUID.fromString("b03901e4-710c-4509-9718-a3d15882d050");
+        UUID BW_UUID = UUID.fromString("aa401ee7-3bb2-410c-9dda-2128726513a1");
 
-        public static final UUID destination_MMSocket_UUID = UUID.fromString("fa249bcd-e53c-4965-a9f9-d7ea5d6f0040");
-        public static final UUID destination_ACK_UUID = UUID.fromString("d9c13848-d7be-48a1-ac11-5f0c082791c7");
-        public static final UUID destination_BW_UUID = UUID.fromString("5c6ae5f9-cb04-4a71-9552-ffe426b02b99");
+        UUID destination_MMSocket_UUID = UUID.fromString("fa249bcd-e53c-4965-a9f9-d7ea5d6f0040");
+        UUID destination_ACK_UUID = UUID.fromString("d9c13848-d7be-48a1-ac11-5f0c082791c7");
+        UUID destination_BW_UUID = UUID.fromString("5c6ae5f9-cb04-4a71-9552-ffe426b02b99");
     }
 
     public interface DeviceNames {
-        public static final String originDevice = "DTN-PLEGAR1762212642";
-        public static final String secondRouterDevice = "DTN-1641b121";
-        public static final String thirdRouterDevice = "DTN-51a33087";
-        public static final String destinationDevice = "DTN-5da9d6090804";
+        String originDevice = "DTN-PLEGAR1762212642";
+        String secondRouterDevice = "DTN-1641b121";
+        String thirdRouterDevice = "DTN-51a33087";
+        String destinationDevice = "DTN-5da9d6090804";
 
     }
 
     public interface DeviceTypes {
-        public static final String SERVER = "SERVER";
-        public static final String CLIENT = "CLIENT";
+        String SERVER = "SERVER";
+        String CLIENT = "CLIENT";
     }
 
     public interface Miscellaneous {
-        public static final String NONE = "*";
-        public static final int MAX_BANDWIDTH = 300; //To set max 300 KBps
-        public static final String BW_FileSize = "64 KB";
-        public static final int BW_TIME_INTERVAL = 200;
-        public String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String NONE = "*";
+        int MAX_BANDWIDTH = 300; //To set max 300 KBps
+        String BW_FileSize = "64 KB";
+        int BW_TIME_INTERVAL = 200;
+        String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     }
 }
